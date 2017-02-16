771c37795b362f4a5570ecbc2e704a075a2e4510
rjhallsted <rjhallsted@gmail.com>
2017-02-16 15:25:20 -0700

    Much more compact version of even fibbonacci.

--- even_fibonacci.rb
+++ even_fibonacci.rb
@@ -1,18 +1,12 @@
-def increment_fibonacci( fibonnaci_array )
-  temp_last_number = fibonnaci_array.first
-  fibonnaci_array[0] = fibonnaci_array.first + fibonnaci_array.last
-  fibonnaci_array[-1] = temp_last_number
-  return fibonnaci_array
-end
-
-four_million = 1000 * 4000
-fibonnaci_array = [1,0]
-
-even_fibonnaci_sum = 0
-while fibonnaci_array.first < four_million do
-  if fibonnaci_array.first.even?
-    even_fibonnaci_sum += fibonnaci_array.first
+class Integer
+  def million
+    self * 1000 * 1000
   end
-  fibonnaci_array = increment_fibonacci( fibonnaci_array )
 end
-print even_fibonnaci_sum.to_s + "\n"
+
+fib_array = [0,1]
+while fib_array.last < 4.million
+  fib_array.push( fib_array[-2] + fib_array[-1] )
+end
+evens = fib_array.select { |i| i.even? }
+puts evens.inject(0) { |sum, i| sum + i }
