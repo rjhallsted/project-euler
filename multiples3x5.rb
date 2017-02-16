a1869aed6a3a7f9766a5d7bd26acd265f12c3175
rjhallsted <rjhallsted@gmail.com>
2017-02-16 14:52:10 -0700

    Added clever solution to multiples of 3 and 5 problem.

--- multiples3x5.rb
+++ multiples3x5.rb
@@ -1,7 +1,12 @@
+#readable
 sum = 0;
 for i in 1..999
   if i % 3 == 0 || i % 5 == 0
     sum += i
   end
 end
-print sum.to_s + "\n"
+puts sum
+
+#clever
+modded = [*1..999].select { |i| i % 3 == 0 || i % 5 == 0 }
+puts modded.inject(0) { |sum,i| sum + i }
